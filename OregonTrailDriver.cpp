a97a5028d8f38e3d9e1ccdd00e3979d6894f5280
Liam Smith <liam@Liams-MacBook-Pro.local>
2020-11-28 19:31:56 -0500

    removed duplicate leader name

--- OregonTrailDriver.cpp
+++ OregonTrailDriver.cpp
@@ -14,7 +14,7 @@ int main(){
     cout<<"THIS PROGRAM SIMULATES A TRIP OVER THE OREGON TRAIL FROMINDEPENDENCE, MISSOURI TO OREGON CITY, OREGON IN 1847. YOUR FAMILY OF FIVE WILL COVER THE 2040 MILE OREGON TRAIL IN 5-6MONTHS --- IF YOU MAKE IT ALIVE."<<endl<<endl;
     Settlers settlers;
     settlers.inputLeaderName();
-    cout<<settlers.getLeaderName()<<endl;
+    //cout<<settlers.getLeaderName()<<endl;
     settlers.inputCompanionNames();
     settlers.printCompanionNames();
     settlers.printMoney();
