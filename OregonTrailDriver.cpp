4bdb5edfebe2acc988d5358d59400be4845f0c59
Liam Smith <liam@Liams-MacBook-Pro.local>
2020-11-30 16:17:56 -0500

    adding turns

--- OregonTrailDriver.cpp
+++ OregonTrailDriver.cpp
@@ -4,15 +4,17 @@
 // Project 3
 #include "Settlers.h"
 #include "Store.h"
+#include "Calender.h"
 #include <iostream>
 #include <cmath>
 using namespace std;
 
 
 int main(){
-    
+    int health = 1;
     cout<<"THIS PROGRAM SIMULATES A TRIP OVER THE OREGON TRAIL FROMINDEPENDENCE, MISSOURI TO OREGON CITY, OREGON IN 1847. YOUR FAMILY OF FIVE WILL COVER THE 2040 MILE OREGON TRAIL IN 5-6MONTHS --- IF YOU MAKE IT ALIVE."<<endl<<endl;
     Settlers settlers;
+    Month date;
     settlers.inputLeaderName();
     //cout<<settlers.getLeaderName()<<endl;
     settlers.inputCompanionNames();
@@ -23,6 +25,13 @@ int main(){
     Store store(1.0);
     settlers.visitStore(store);
     settlers.printSupplies();
+    while (health!=0)
+    {
+        cout<<date.computeCurrentDate()<<endl;
+        settlers.printMilesToGo();
+        
+        health = 0;
+    }
     //settlers.printMoney();
     //store.printMenu();
     //buying items in store
